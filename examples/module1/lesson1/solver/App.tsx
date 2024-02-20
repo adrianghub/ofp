e70636c96c2bac579f673d8b2476fd14f5201ac2
Adrian Zinko <zinko.adrian00@gmail.com>
2024-02-20 19:09:17 +0100

    solution - examples/module1/lesson1/solver

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { f1, f2, f3, f4 } from './functions';
+import { useState } from 'react';
+import { add, divide, multiply, subtract } from './functions';
 
 const App = () => {
-  const [numA, setNumA] = useState<number>(0);
-  const [numB, setNumB] = useState<number>(0);
-  const [numC, setNumC] = useState<number | string>(0);
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [result, setResult] = useState(0);
 
-  const doWork = (func: (a: number, b: number) => number) => {
-    setNumC(func(numA, numB));
+  const performCalculation = (
+    calculateCb: (a: number, b: number) => number
+  ) => {
+    setResult(calculateCb(firstNumber, secondNumber));
   };
 
   return (
@@ -16,45 +18,46 @@ const App = () => {
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={numA}
-          onChange={(e) => setNumA(parseFloat(e.target.value))}
+          value={firstNumber}
+          onChange={(e) => setFirstNumber(parseFloat(e.target.value))}
         />
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={numB}
-          onChange={(e) => setNumB(parseFloat(e.target.value))}
+          value={secondNumber}
+          onChange={(e) => setSecondNumber(parseFloat(e.target.value))}
         />
       </div>
       <div className="grid grid-cols-4 gap-x-4 my-4">
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f1)}
-        >
-          +
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f2)}
-        >
-          -
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f3)}
-        >
-          *
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f4)}
-        >
-          /
-        </button>
+        <CalculatorButton onClick={() => performCalculation(add)} sign="+" />
+        <CalculatorButton
+          onClick={() => performCalculation(subtract)}
+          sign="-"
+        />
+        <CalculatorButton
+          onClick={() => performCalculation(multiply)}
+          sign="*"
+        />
+        <CalculatorButton onClick={() => performCalculation(divide)} sign="/" />
       </div>
-      <div>Result: {numC}</div>
+      <div>Result: {result}</div>
     </div>
   );
 };
 
 export default App;
+
+const CalculatorButton = ({
+  onClick,
+  sign,
+}: {
+  onClick: () => void;
+  sign: string;
+}) => (
+  <button
+    className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+    onClick={onClick}
+  >
+    {sign}
+  </button>
+);
